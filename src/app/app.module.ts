2c0a37a0d085bcbf3e1cff621cd20dd69eb42a8f
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2024-05-21 09:30:15 +0100

    first commit and first day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './components/my-first-component/my-first-component.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { BlogComponent } from './components/site/blog/blog.component';
+import { SidebarComponent } from './components/site/sidebar/sidebar.component';
+import { ListOfUsersComponent } from './components/site/list-of-users/list-of-users.component';
+import { ListOfProductsComponent } from './components/site/list-of-products/list-of-products.component';
+import { ProductCardComponent } from './components/site/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent,
+    NavbarComponent,
+    FooterComponent,
+    BlogComponent,
+    SidebarComponent,
+    ListOfUsersComponent,
+    ListOfProductsComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
